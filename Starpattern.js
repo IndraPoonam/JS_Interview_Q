806c0fc95c83c5e50f9526944095661d871de709
IndraPoonam <iamindra.poonam@gmail.com>
2024-04-05 09:13:24 +0530

    practice js

---
+++ Starpattern.js
@@ -0,0 +1,10 @@
+for(let i=1; i<=5; i++){
+    for(let k=1; k<=i; k++){
+        document.write("&nbsp&nbsp;")
+    }
+    for(let j=1; j<=5; j++){
+        document.write("*"," ")
+    }
+  
+    document.write("<br>")
+}
