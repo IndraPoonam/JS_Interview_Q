b93f21a1da5973670fd3c50f42565cd97fe60886
IndraPoonam <iamindra.poonam@gmail.com>
2024-05-20 14:39:14 +0530

    qurstion  practice

---
+++ data.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)        //we are using nodejs not browser
+
+
+
+
+// console.log(typeof(4));
+// console.log(typeof(null));        //object
+// console.log(typeof undefined);      //undefined
+
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let score ="33"
+// let score ="33ab";                            //when we convert 33ab to number  its value is NaN    
+// let score = null;                     //when we convert null to number then its value is zero
+console.log(typeof score)
+let valueInNumber = Number(score);                //Number is use to convert string to number
+// console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
